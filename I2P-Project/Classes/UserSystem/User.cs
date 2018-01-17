79cf8f71072b21c039cb330b0e3d6a29b45b3588
Пётр <calah48@yandex.ru>
2018-01-17 13:14:40 +0300

    Managed folder architecture a bit

---
+++ I2P-Project/Classes/UserSystem/User.cs
@@ -0,0 +1,14 @@
+﻿namespace UserSystem
+{
+
+    // TODO Make integration with DB and getters by ID
+    public abstract class User
+    {
+
+        public int ID { get; private set; }
+
+        public string Name { get; private set; }
+
+    }
+
+}

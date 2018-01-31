438b12086752eab6eebad4957ac7f0b64ad3fb4e
Anton Krylov <alenton11@yandex.ru>
2018-01-31 13:45:16 +0300

    It works

--- Classes/Documents/Document.cs
+++ Classes/Documents/Document.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,6 +15,7 @@ namespace I2P_Project.Classes.Documents
         private bool isBesteller; //2 weeks for check out
         private DateTime timeOfCheckOut;
         private int personID;
+        private int bookType;
         //TODO
             //Connect with DB
         
---
+++ DataBases/dbo.DocumentsDB.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[DocumentsDB] (
+    [PersonID]          INT  DEFAULT ((0)) NOT NULL,
+    [Title]        TEXT NOT NULL,
+    [Description]      TEXT NOT NULL,
+    [Price] TEXT NOT NULL,
+    [IsBesteller]    INT  NOT NULL,
+    [TimeOfCheckOut] INT  NULL,
+	[BookType] INT NOT NULL,
+);

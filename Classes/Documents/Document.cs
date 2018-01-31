b4c44df9c3771eef622ef35baa1d01a92d2a0e17
Пётр <calah48@yandex.ru>
2018-01-31 11:04:30 +0300

    Fixed structure of folders

--- Classes/Documents/Document.cs
+++ Classes/Documents/Document.cs
@@ -8,19 +8,14 @@ namespace I2P_Project.Classes.Documents
 {
     abstract class Document
     {
-        private String title;
-        private String description;
+        private string title;
+        private string description;
         private int price; //in rubles
         private bool isBesteller; //2 weeks for check out
         private DateTime timeOfCheckOut;
         private int personID;
         //TODO
             //Connect with DB
-        public string Description { get => description; set => description = value; }
-        public string Title { get => title; set => title = value; }
-        public int Price { get => price; set => price = value; }
-        public bool IsBesteller { get => isBesteller; set => isBesteller = value; }
-        public DateTime TimeOfCheckOut { get => timeOfCheckOut; set => timeOfCheckOut = value; }
-        public int PersonID { get => personID; set => personID = value; }
+        
     }
 }

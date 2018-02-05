20acb289b1030e17d9fbb48e04c05c0b57b62544
user <user@USER->
2018-02-05 21:38:04 +0300

    test casez

--- Classes/Data Managers/DataBaseManager.cs
+++ Classes/Data Managers/DataBaseManager.cs
@@ -109,5 +109,11 @@ namespace I2P_Project.Classes.Data_Managers
             return 100;
         }
 
+
+        public static void ClearDB()
+        {
+            //TODO
+        }
+
     }
 }
---
+++ Tests/Test.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using I2P_Project.Classes.Documents;
+using I2P_Project.Classes.UserSystem;
+using I2P_Project.Classes.Data_Managers;
+namespace I2P_Project.Tests
+{
+    class Test
+    {
+        public bool test1()
+        {
+
+            DataBaseManager.ClearDB();
+
+
+            DataBaseManager.RegisterUser("st", "st", "st", "st", "st", false);
+            DataBaseManager.RegisterUser("lb", "lb", "lb", "lb", "lb", true);
+
+
+
+            SystemDataManager.CurrentUser = new Student("st");
+
+            return true;
+        }
+    }
+}

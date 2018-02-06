6a072ff17604dc4955fe0b192f4d6d5da6113a3a
Пётр <calah48@yandex.ru>
2018-02-06 16:34:59 +0300

    Librarian home page

---
+++ Pages/LibrarianHomePage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace I2P_Project.Pages
+{
+    /// <summary>
+    /// Interaction logic for LibrarianHomePage.xaml
+    /// </summary>
+    public partial class LibrarianHomePage : Window
+    {
+        public LibrarianHomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void UserManagementClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DocumentsManagementClick(object sender, RoutedEventArgs e)
+        {
+            // TODO
+        }
+    }
+}

90e340c0c62b5c79e74b3f6afd649d4e229b4159
Пётр <calah48@yandex.ru>
2018-02-07 13:28:26 +0300

    fixed tests AGAIN

--- Classes/UserSystem/Student.cs
+++ Classes/UserSystem/Student.cs
@@ -12,24 +12,10 @@ namespace I2P_Project.Classes.UserSystem
             CheckedDocs = new List<int>();
             SetCurrent(eMail);
         }
-        
-        public override string CheckOut(int docID)
+
+        public override string CheckOut(string title)
         {
-            document doc = DataBaseManager.GetFreeCopy(docID);
-            if (doc == null) return "Book is not availible for now, please come back later";
-            int user_id = SystemDataManager.CurrentUser.PersonID;
-
-            if (doc.IsBestseller || doc.DocType != 0)
-                DataBaseManager.SetCheckOut(docID, user_id, 2);
-            else
-                DataBaseManager.SetCheckOut(docID, user_id, 3);
-
-            return "Checked out " + doc.Title + " successfully!";
-        }
-
-        public override string CheckOut(string author)
-        {
-            document doc = DataBaseManager.GetFreeCopy(author);
+            document doc = DataBaseManager.GetFreeCopy(title);
             if (doc == null) return "Book is not availible for now, please come back later";
             int user_id = SystemDataManager.CurrentUser.PersonID;
 
